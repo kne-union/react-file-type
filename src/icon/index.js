d5502a27176f87d66918a3caf038c5b0ddbcb8a9
zhipenglin <sunandmoon001@163.com>
2022-11-01 09:17:41 +0800

    提交一下

--- src/icon/index.js
+++ src/icon/index.js
@@ -1,5 +1,6 @@
 import DocIcon from './doc';
 import DocxIcon from './docx';
+import FullIcon from './full';
 import GifIcon from './gif';
 import JpgIcon from './jpg';
 import Mp4Icon from './mp4';
@@ -8,6 +9,7 @@ import PngIcon from './png';
 import PptIcon from './ppt';
 import PptxIcon from './pptx';
 import RarIcon from './rar';
+import RotateIcon from './rotate';
 import SvgIcon from './svg';
 import UnknowIcon from './unknow';
 import XlsIcon from './xls';
@@ -17,6 +19,7 @@ import ZipIcon from './zip';
 export default {
   "doc":DocIcon,
   "docx":DocxIcon,
+  "full":FullIcon,
   "gif":GifIcon,
   "jpg":JpgIcon,
   "mp4":Mp4Icon,
@@ -25,6 +28,7 @@ export default {
   "ppt":PptIcon,
   "pptx":PptxIcon,
   "rar":RarIcon,
+  "rotate":RotateIcon,
   "svg":SvgIcon,
   "unknow":UnknowIcon,
   "xls":XlsIcon,
